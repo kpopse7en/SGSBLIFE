225f45b1c755975c7a19eb2137984ee8e0588d0b
kpopse7en <yur.vargas@alumnos.duoc.cl>
2018-04-10 22:55:39 -0300

    ALGUNOS CAMBIOS PREVIAMENTE APLICADOS

---
+++ EJEMPLO.v1.1/Biblioteca.Entidades/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;//permite acceder a los archivos del sistema
+namespace Biblioteca
+{
+    public class Logger
+    {
+        public static void Mensaje(String msg)
+        {
+            msg =
+                DateTime.Now + " | " + msg + Environment.NewLine;
+            File.AppendAllText(@"D:\Log.txt", msg);
+        }
+    }
+}
---
+++ EJEMPLO.v1.1/Biblioteca.Entidades/Plan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca.DALC;
+namespace Biblioteca.Entidades
+{
+    public class Plan
+    {
+        public String Id { get; set; }
+        public String Nombre { get; set; }
+        public Double PrimaBase { get; set; }
+        public String PolizaActual { get; set; }
+        Biblioteca.DALC.BeLifeEntities Entidades;//inicaliza
+        public Plan()
+        {
+            Entidades = new Biblioteca.DALC.BeLifeEntities();
+        }
+
+    }
+}
